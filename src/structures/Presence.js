e4772f456e0b901b9fc50c410ae0d8c9510554a9
Sorunome <mail@sorunome.de>
2020-01-13 17:11:23 +0100

    properly fix presence checking

--- src/structures/Presence.js
+++ src/structures/Presence.js
@@ -55,8 +55,8 @@ class Presence {
       presence &&
       this.status === presence.status &&
       (this.game ? this.game.equals(presence.game) : !presence.game) &&
-      ((this.clientStatus === null && presence.clientStatus === null) ||
-      (this.clientStatus !== null && presence.clientStatus !== null &&
+      ((!this.clientStatus && !presence.clientStatus) ||
+      (this.clientStatus && presence.clientStatus &&
       this.clientStatus.web === presence.clientStatus.web &&
       this.clientStatus.mobile === presence.clientStatus.mobile &&
       this.clientStatus.desktop === presence.clientStatus.desktop))
